8aba8400d4d2120dd0f0adcc1e56a48428819452
AaronHopkins <aaronhopkins@live.co.uk>
2026-02-19 18:24:22 +0000

    Animation of Player
    Added the player idle and running animation

--- RPG Combat System/Assets/Scripts/PlayerController.cs
+++ RPG Combat System/Assets/Scripts/PlayerController.cs
@@ -5,13 +5,16 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float moveSpeed = 1f;
     private Vector2 movement;
     private Rigidbody2D rb;
+    private Animator myAnimator;
+    private SpriteRenderer mySpriteRenderer;
 
-    
     private PlayerControls playerControls;
 
     private void Awake() {
         playerControls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -21,6 +24,7 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
+        AdjustPlatyerFacingDirection();
         Move();
     }
     private void OnEnable() {
@@ -30,10 +34,29 @@ public class PlayerController : MonoBehaviour
     private void PlayerInput()
     {
         movement = playerControls.Movement.Move.ReadValue<Vector2>();
+
+        myAnimator.SetFloat("moveX", movement.x);
+        myAnimator.SetFloat("moveY", movement.y);
     }
 
     private void Move()
     {
         rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
     }
+
+    private void AdjustPlatyerFacingDirection()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+        
+        if(mousePos.x < playerScreenPoint.x)
+        {
+            mySpriteRenderer.flipX = true;
+        }
+        else
+        {
+            mySpriteRenderer.flipX = false;
+        }
+    }
 }
+ 
