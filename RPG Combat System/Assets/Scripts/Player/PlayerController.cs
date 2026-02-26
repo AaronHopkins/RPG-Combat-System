fbaa16cf4af0802db41c4cdb3c7236be7ae20c83
AaronHopkins <aaronhopkins@live.co.uk>
2026-02-26 15:48:29 +0000

    Enemy Health and Knockback
    Got it so that the enemies are able to be killed by the player and as feedback mechanic got it so that the enemies are able to be knocked back and have a flash of white to simulate damage.

---
+++ RPG Combat System/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public bool FacingLeft { get { return facingLeft;  } set { facingLeft = value; } }
+    public static PlayerController Instance;
+
+    [SerializeField] private float moveSpeed = 1f;
+
+    private Vector2 movement;
+    private Rigidbody2D rb;
+    private Animator myAnimator;
+    private SpriteRenderer mySpriteRenderer;
+    private PlayerControls playerControls;
+
+    private bool facingLeft = false;
+
+    private void Awake() {
+        Instance = this;
+        playerControls = new PlayerControls();
+        rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        PlayerInput();
+    }
+
+    void FixedUpdate()
+    {
+        AdjustPlatyerFacingDirection();
+        Move();
+    }
+    private void OnEnable() {
+        playerControls.Enable();
+    }
+
+    private void PlayerInput()
+    {
+        movement = playerControls.Movement.Move.ReadValue<Vector2>();
+
+        myAnimator.SetFloat("moveX", movement.x);
+        myAnimator.SetFloat("moveY", movement.y);
+    }
+
+    private void Move()
+    {
+        rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+    }
+
+    private void AdjustPlatyerFacingDirection()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+        
+        if(mousePos.x < playerScreenPoint.x)
+        {
+            mySpriteRenderer.flipX = true;
+            FacingLeft = true;
+        }
+        else
+        {
+            mySpriteRenderer.flipX = false;
+            FacingLeft = false;
+        }
+    }
+}
+ 
---
+++ RPG Combat System/Assets/Scripts/Player/Sword.cs
@@ -0,0 +1,94 @@
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+    [SerializeField] private GameObject slahsAnimPrefab;
+    [SerializeField] private Transform slashAnimSpawnPoint;
+    [SerializeField] private Transform weaponCollider;
+
+    private PlayerControls playerControls;
+    private Animator myAnimator;
+    private PlayerController playerController;
+    private ActiveWeapon activeWeapon;
+
+    private GameObject slashAnim;
+
+    private void Awake()
+    {
+        activeWeapon = GetComponentInParent<ActiveWeapon>();
+        playerController = GetComponentInParent<PlayerController>();
+        playerControls = new PlayerControls();
+        myAnimator = GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    void Start()
+    {
+        playerControls.Combat.Attack.started += _ => Attack();
+    }
+
+    private void Attack()
+    {
+        myAnimator.SetTrigger("Attack");
+
+        weaponCollider.gameObject.SetActive(true);
+
+        slashAnim = Instantiate(slahsAnimPrefab, slashAnimSpawnPoint.position, Quaternion.identity);
+        slashAnim.transform.parent = this.transform.parent;
+    }
+
+    public void DoneAttackingEvent()
+    {
+        weaponCollider.gameObject.SetActive(false);
+    }
+
+    public void SwingUpFlipAnimEventS()
+    {
+        slashAnim.gameObject.transform.rotation = Quaternion.Euler(-180, 0, 0);
+        
+        if(playerController.FacingLeft)
+        {
+            slashAnim.GetComponent<SpriteRenderer>().flipX = true;
+        }
+    }
+    public void SwingDownFlipAnimEvent()
+    {
+        slashAnim.gameObject.transform.rotation = Quaternion.Euler( 0, 0, 0);
+
+        if (playerController.FacingLeft)
+        {
+            slashAnim.GetComponent<SpriteRenderer>().flipX = true;
+        }
+    }
+
+    private void Update()
+    {
+        MouseFollowWithOffset();
+    }
+
+    private void MouseFollowWithOffset()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerPos = playerController.transform.position;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(playerPos);
+        
+
+        float angle = Mathf.Atan2(mousePos.y - playerPos.y, Mathf.Abs(mousePos.x - playerPos.x)) * Mathf.Rad2Deg;
+
+        if(mousePos.x < playerScreenPoint.x)
+        {
+            activeWeapon.transform.rotation = Quaternion.Euler(0, -180, angle);
+            weaponCollider.transform.rotation = Quaternion.Euler(0, -180, 0);
+            
+        }
+        else
+        {
+            activeWeapon.transform.rotation = Quaternion.Euler(0, 0, angle);
+            weaponCollider.transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
+}
